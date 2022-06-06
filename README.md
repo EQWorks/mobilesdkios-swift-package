f0abafdf3abaea05cb31df337e8c12a5e2ef8780
sb689 <sujata689@gmail.com>
2022-06-06 15:45:09 -0500

    feature- added mobilesdkios xcframework to this package for distribution
    Creating a new repo to distribute mobilesdkios

---
+++ README.md
@@ -0,0 +1,3 @@
+# mobilesdkios-swift-package
+
+A description of this package.
---
+++ Sources/mobilesdkios.xcframework/ios-arm64_x86_64-maccatalyst/mobilesdkios.framework/Versions/A/Headers/mobilesdkios.h
@@ -0,0 +1,18 @@
+//
+//  mobilesdkios.h
+//  mobilesdkios
+//
+//  Created by sujata baral on 2022-06-06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mobilesdkios.
+FOUNDATION_EXPORT double mobilesdkiosVersionNumber;
+
+//! Project version string for mobilesdkios.
+FOUNDATION_EXPORT const unsigned char mobilesdkiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mobilesdkios/PublicHeader.h>
+
+
